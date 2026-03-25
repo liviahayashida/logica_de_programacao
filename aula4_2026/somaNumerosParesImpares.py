80d5966443013536daada0444f0f32bf5ffe05d2
Livia Hayashida <livia.hayshida@aluno.senai.br>
2026-03-25 16:30:06 -0300

    Fix input prompt and add sum calculations for evens and odds

--- aula4_2026/somaNumerosParesImpares.py
+++ aula4_2026/somaNumerosParesImpares.py
@@ -1,2 +1,11 @@
 print("Este programa soma os pares e os impares de 1 até o número selecionado")
-num = int(input("Digite"))
+num = int(input("Digite um numero: "))
+pares = 0
+impares = 0
+for i in range(1, numero + 1):
+    if i % 2 == 0:
+        pares += i
+    else:
+        impares += i
+print("Soma dos pares:", pares)
+print("Soma dos ímpares:", impares)
