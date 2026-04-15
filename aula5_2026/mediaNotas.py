daf0233786945a4f3c92ebbc010fc17275c8615d
Livia Hayashida <livia.hayshida@aluno.senai.br>
2026-04-15 09:28:04 -0300

    Implement average grade calculation
    Add functionality to calculate average of grades.

--- aula5_2026/mediaNotas.py
+++ aula5_2026/mediaNotas.py
@@ -1,1 +1,11 @@
 print("Este programa calcula a média de suas notas")
+
+nota = 0
+soma = 0
+quantidade = 0 
+while nota >=0:
+    nota = int(input("Digite sua nota: "))
+    if nota>=0:
+        soma = soma+nota
+        quantidade= quantidade+1
+        print(f"Sua media atual é de: {soma/quantidade}")
